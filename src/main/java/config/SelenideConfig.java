af7b72169f0989264faab64b7d94c5530e7222d4
Oleksandr Stepanov <oleksandr.stepanov.qa@gmail.com>
2021-10-04 01:03:22 +0300

    Added the connection to Selenoid

--- src/main/java/config/SelenideConfig.java
+++ src/main/java/config/SelenideConfig.java
@@ -1,11 +1,16 @@
 package config;
 
 import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class SelenideConfig {
     public static void createBrowserConfig(String browser) {
-
+        Configuration.remote = "http://localhost:4444/wd/hub";
         Configuration.browser = browser;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC",true);
+        capabilities.setCapability("enableVideo",true);
+        Configuration.browserCapabilities = capabilities;
         Configuration.fastSetValue = false;
         Configuration.headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
         Configuration.pageLoadStrategy = "eager";
